480b69f7aedc2b12dae9cf645a1dc249228308c2
Chris <comosjay@gmail.com>
2024-11-28 16:14:58 -0800

    done

--- Portfolio/src/Pages/Welcome/welcome.tsx
+++ Portfolio/src/Pages/Welcome/welcome.tsx
@@ -9,14 +9,19 @@ import { IoMailOutline } from "react-icons/io5";
 import {motion} from 'framer-motion';
 import Resume from '../../Resume.pdf'
 function Welcome(props:any) {
-  const MainAnimation = {
+  const MainAnimation = { 
     hidden: { opacity: 0, y: 10 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.8, staggerChildren: 0.2 } },
-  };
+    
+  }; 
 
   const subAnimation = {
     hidden: { opacity: 0, y: 5 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5, staggerChildren: 0.1,} },
+  };
+  const itemVariants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.7 , type: "spring", stiffness: 150, damping: 10} }, 
   };
   return (
     <> 
@@ -36,12 +41,21 @@ function Welcome(props:any) {
             </motion.div>
           <Anitext text = "I'm Seojin/Chris Park" class = "header"/>
           <motion.div className='Options' variants={subAnimation}>
-              <DefaultButton text = "LinkedIn" color = "Blue" icon = {<FaLinkedin size={22}/>} link = 'https://www.linkedin.com/in/seojin-park-332aaa213/'/>
-              <DefaultButton text = "GitHub" color = "Black"  icon = {<FiGithub size={22}/>} link = 'https://github.com/qwer030413'/>
-              <DefaultButton text = "Instagram" color = "Yellow" icon = {<FaInstagram size={22}/>} link = 'https://www.instagram.com/chrispark03_/'/>
-              <DefaultButton text = "Email" color = "Green" icon = {<IoMailOutline size={22}/>} link = "mailto:comosjay@gmail.com?subject=(Subject)&body=Hi%20Chris!" />
-
-              <DefaultButton text = "Resume" color = "Red" icon = {<IoDocumentTextOutline size={22}/>} link = {Resume}/>
+              <motion.div variants={itemVariants}>
+                <DefaultButton text = "LinkedIn" color = "Blue" icon = {<FaLinkedin size={22}/>} link = 'https://www.linkedin.com/in/seojin-park-332aaa213/'/>
+              </motion.div>
+              <motion.div variants={itemVariants}>
+                <DefaultButton text = "GitHub" color = "Black"  icon = {<FiGithub size={22}/>} link = 'https://github.com/qwer030413'/>
+              </motion.div>
+              <motion.div variants={itemVariants}>
+                <DefaultButton text = "Instagram" color = "Yellow" icon = {<FaInstagram size={22}/>} link = 'https://www.instagram.com/chrispark03_/'/>
+              </motion.div>
+              <motion.div variants={itemVariants}>
+                <DefaultButton text = "Email" color = "Green" icon = {<IoMailOutline size={22}/>} link = "mailto:comosjay@gmail.com?subject=(Subject)&body=Hi%20Chris!" />
+              </motion.div>
+              <motion.div variants={itemVariants}>
+                <DefaultButton text = "Resume" color = "Red" icon = {<IoDocumentTextOutline size={22}/>} link = {Resume}/>
+              </motion.div>
           </motion.div>
         </div>
       </motion.div>
